5c51de525db6ad118729ad3c2a7e021b6e543e4d
dhksh16 <dhksh16@daum.net>
2015-07-31 14:07:24 +0900

    Create CheckNums.rb

---
+++ CheckNums.rb
@@ -0,0 +1,16 @@
+def CheckNums(num1,num2)
+
+  # code goes here
+  
+  if num1 == num2
+    return -1
+    
+  elsif num1 > num2
+    return false
+    
+  elsif num1 < num2
+    return true
+    
+  end
+         
+end
